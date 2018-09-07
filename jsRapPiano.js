d29b0e99cf2772acbb5106e3c65a320380430550
Thibor <thibor@vp.pl>
2018-09-07 07:29:34 +0100

    Add files via upload

---
+++ jsRapPiano.js
@@ -0,0 +1,82 @@
+AudioContext = window.AudioContext || window.webkitAudioContext,
+audioCtx = new AudioContext(),
+oscillator = audioCtx.createOscillator(),
+gainNode = audioCtx.createGain();
+oscillator.start(0);
+gainNode.gain.value = 0;
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+
+$(window).resize(function(){
+$('.rapPiano').each(function(){
+	this.Render();
+});
+});
+	
+(function($){
+$.fn.jsRapPiano = function(options){
+
+return this.each(function(){
+	
+	this.opt = $.extend({
+		octave:3,
+		octaves:2,
+		attackTime:0.04,
+		releaseTime:0.5,
+		waveType:'square',
+		onClick:null
+	},options);
+	let base = this;
+	oscillator.type = this.opt.waveType;
+	oscillator.frequency.value = 50;
+	
+	this.Render = function(){
+		$(this).empty();
+		let w = $(this).width();
+		w = w / (this.opt.octaves * 7);
+		$(this).addClass('rapPiano').height(w * 5);
+		let i = 12 * (this.opt.octave + 1);
+		for(let o = 0;o < this.opt.octaves;o++)
+			for(let x = 0;x < 7;x++){
+				let k = $('<div>').addClass('divKey').css({width:w}).appendTo(this);
+				$('<div>').addClass('major').prop('index',i++).appendTo(k);
+				if((x % 7 == 2) || (x % 7 == 6))continue;
+				$('<div>').addClass('minor').prop('index',i++).appendTo(k);
+			}
+		$('.major,.minor',this).bind({
+			mousedown:function(e){
+				let i = $(this).prop('index');
+				let f =440 * Math.pow(2,(i - 69) / 12);
+				base.PlaySound(f);
+				if(base.opt.onClick)
+					base.opt.onClick.call(base);
+			}
+		});	
+	}
+	
+	this.PlaySound = function(frequency){
+		oscillator.frequency.value = frequency;
+		let now = audioCtx.currentTime;
+		//gainNode.gain.setTargetAtTime(0,now,0.05);
+		gainNode.gain.setTargetAtTime(1,now,this.opt.attackTime);
+		gainNode.gain.setTargetAtTime(0,now + this.opt.attackTime,this.opt.releaseTime);
+		
+		/*let now = audioCtx.currentTime;
+		gainNode.gain.setValueAtTime(0,now + 0.05);
+		//oscillator.frequency.value = frequency;
+		gainNode.gain.setValueAtTime(1,now + 0.1);
+		gainNode.gain.setTargetAtTime(0, now + 0.1,this.opt.releaseTime);*/
+		
+		//oscillator.frequency.value = frequency;
+		/*let now = audioCtx.currentTime;
+		gainNode.gain.cancelScheduledValues(now);
+		gainNode.gain.setValueAtTime(0, now);
+		gainNode.gain.linearRampToValueAtTime(1, now + 0.1);
+		gainNode.gain.linearRampToValueAtTime(0, now + 0.1 + this.opt.releaseTime);*/
+	}
+	
+	this.Render();
+
+})
+
+}})(jQuery);
